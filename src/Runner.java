977ca5c540785159758033def6b7463eee31ea63
unknown <BT_4N2_11@BKTECH.LOCAL>
2022-11-07 12:29:13 -0500

    worked on nested iteration

---
+++ src/Runner.java
@@ -0,0 +1,14 @@
+public class Runner {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i++)
+        {
+            for (int j = 3; j > 1; j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
